87bfbe22af96f7f521180b1e40fa27e4911de54f
Max <pichonnama@gmail.com>
2016-04-27 15:36:51 +0200

    Added files via upload

---
+++ Modules/mTirBalistique/Tire-Balistique.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Tire_balistique
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBoxSpeed = new System.Windows.Forms.TextBox();
+            this.textBoxDegrees = new System.Windows.Forms.TextBox();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.labelDegrees = new System.Windows.Forms.Label();
+            this.timerTemp = new System.Windows.Forms.Timer(this.components);
+            this.panelDraw = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonStop);
+            this.groupBox1.Controls.Add(this.textBoxSpeed);
+            this.groupBox1.Controls.Add(this.textBoxDegrees);
+            this.groupBox1.Controls.Add(this.labelSpeed);
+            this.groupBox1.Controls.Add(this.labelDegrees);
+            this.groupBox1.Location = new System.Drawing.Point(12, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(671, 87);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Saisie des paramètre du tire balistique";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(489, 30);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(125, 32);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Départ";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBoxSpeed
+            // 
+            this.textBoxSpeed.Location = new System.Drawing.Point(283, 37);
+            this.textBoxSpeed.Name = "textBoxSpeed";
+            this.textBoxSpeed.Size = new System.Drawing.Size(57, 20);
+            this.textBoxSpeed.TabIndex = 3;
+            // 
+            // textBoxDegrees
+            // 
+            this.textBoxDegrees.Location = new System.Drawing.Point(123, 37);
+            this.textBoxDegrees.Name = "textBoxDegrees";
+            this.textBoxDegrees.Size = new System.Drawing.Size(59, 20);
+            this.textBoxDegrees.TabIndex = 2;
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Location = new System.Drawing.Point(188, 40);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(89, 13);
+            this.labelSpeed.TabIndex = 1;
+            this.labelSpeed.Text = "Vitesse [en m/s]  ";
+            // 
+            // labelDegrees
+            // 
+            this.labelDegrees.AutoSize = true;
+            this.labelDegrees.Location = new System.Drawing.Point(36, 40);
+            this.labelDegrees.Name = "labelDegrees";
+            this.labelDegrees.Size = new System.Drawing.Size(81, 13);
+            this.labelDegrees.TabIndex = 0;
+            this.labelDegrees.Text = "Angle [en Deg] ";
+            // 
+            // timerTemp
+            // 
+            this.timerTemp.Interval = 10;
+            this.timerTemp.Tick += new System.EventHandler(this.timerTemp_Tick);
+            // 
+            // panelDraw
+            // 
+            this.panelDraw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDraw.Location = new System.Drawing.Point(12, 119);
+            this.panelDraw.Name = "panelDraw";
+            this.panelDraw.Size = new System.Drawing.Size(670, 445);
+            this.panelDraw.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 572);
+            this.Controls.Add(this.panelDraw);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Tire Balistique";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textBoxSpeed;
+        private System.Windows.Forms.TextBox textBoxDegrees;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.Label labelDegrees;
+        private System.Windows.Forms.Timer timerTemp;
+        private System.Windows.Forms.Panel panelDraw;
+    }
+}
+
---
+++ Modules/mTirBalistique/Tire-Balistique.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tire_balistique
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool textButton = false;
+
+  
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (textButton == false)
+            {
+                buttonStop.Text = "Pause";
+                textButton = true;
+                timerTemp.Start();
+                calBalistique();
+                
+            }
+            else
+            {
+                buttonStop.Text = "Départ";
+                textButton = false;
+                timerTemp.Stop();
+            }
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Calcule le tire balistique de l'object
+        /// </summary>
+        private void calBalistique()
+        {
+            double GravEarth = -9.81;
+
+            double SpeedY;
+            double SpeedX;
+            double x, y;
+
+            int X = 0;
+            int Y = panelDraw.Height;
+
+            double speed = Convert.ToDouble(textBoxSpeed.Text);
+            double angle = Convert.ToDouble(textBoxDegrees.Text);
+
+
+
+            //Création du graphique
+            Bitmap pen = new Bitmap(4, 4);
+            pen.SetPixel(0, 0, Color.Red);
+            System.Drawing.Graphics graphBali = panelDraw.CreateGraphics();
+
+            //Vy Calcul la vitesse montante
+            SpeedY = Math.Sin(Math.PI * (angle / 180)) * speed;
+            
+
+            //Calcul balistique du tire
+            for (double i = 0; i < 10000; i += 0.01)
+            {
+                //Vx Calcul la vitesse horizontale
+                SpeedX = Math.Cos(Math.PI * (angle / 180)) * speed;
+
+                x = i;
+                y = SpeedY * i + ((0.5 * (GravEarth)) * (i * i));
+
+                graphBali.DrawImageUnscaled(pen, X + Convert.ToInt32(SpeedX * i), Y - Convert.ToInt32(y));
+
+            }
+        }
+
+        //Timer 
+        private void timerTemp_Tick(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
