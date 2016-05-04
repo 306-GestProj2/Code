a286be60788c55de359c709226d763bdd1959c10
Max <pichonnama@gmail.com>
2016-05-04 15:42:19 +0200

    Added files via upload

--- Modules/mTirBalistique/Tire-Balistique.Designer.cs
+++ Modules/mTirBalistique/Tire-Balistique.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonColor = new System.Windows.Forms.Button();
             this.buttonStop = new System.Windows.Forms.Button();
             this.textBoxSpeed = new System.Windows.Forms.TextBox();
             this.textBoxDegrees = new System.Windows.Forms.TextBox();
@@ -37,11 +38,22 @@
             this.labelDegrees = new System.Windows.Forms.Label();
             this.timerTemp = new System.Windows.Forms.Timer(this.components);
             this.panelDraw = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelMaxLenght = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelInstDist = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelMaxHight = new System.Windows.Forms.Label();
+            this.labelHight = new System.Windows.Forms.Label();
+            this.labelHautInst = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.groupBox1.SuspendLayout();
+            this.panelDraw.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.buttonColor);
             this.groupBox1.Controls.Add(this.buttonStop);
             this.groupBox1.Controls.Add(this.textBoxSpeed);
             this.groupBox1.Controls.Add(this.textBoxDegrees);
@@ -54,8 +66,20 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Saisie des paramètre du tire balistique";
             // 
+            // buttonColor
+            // 
+            this.buttonColor.BackColor = System.Drawing.Color.Red;
+            this.buttonColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonColor.Location = new System.Drawing.Point(620, 30);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(18, 33);
+            this.buttonColor.TabIndex = 2;
+            this.buttonColor.UseVisualStyleBackColor = false;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
             // buttonStop
             // 
+            this.buttonStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonStop.Location = new System.Drawing.Point(489, 30);
             this.buttonStop.Name = "buttonStop";
             this.buttonStop.Size = new System.Drawing.Size(125, 32);
@@ -70,6 +94,8 @@
             this.textBoxSpeed.Name = "textBoxSpeed";
             this.textBoxSpeed.Size = new System.Drawing.Size(57, 20);
             this.textBoxSpeed.TabIndex = 3;
+            this.textBoxSpeed.Text = "0";
+            this.textBoxSpeed.TextChanged += new System.EventHandler(this.textBoxSpeed_TextChanged);
             // 
             // textBoxDegrees
             // 
@@ -77,6 +103,8 @@
             this.textBoxDegrees.Name = "textBoxDegrees";
             this.textBoxDegrees.Size = new System.Drawing.Size(59, 20);
             this.textBoxDegrees.TabIndex = 2;
+            this.textBoxDegrees.Text = "0";
+            this.textBoxDegrees.TextChanged += new System.EventHandler(this.textBoxDegrees_TextChanged);
             // 
             // labelSpeed
             // 
@@ -98,16 +126,93 @@
             // 
             // timerTemp
             // 
-            this.timerTemp.Interval = 10;
+            this.timerTemp.Interval = 1;
             this.timerTemp.Tick += new System.EventHandler(this.timerTemp_Tick);
             // 
             // panelDraw
             // 
+            this.panelDraw.BackColor = System.Drawing.Color.White;
             this.panelDraw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDraw.Controls.Add(this.label4);
+            this.panelDraw.Controls.Add(this.labelMaxLenght);
+            this.panelDraw.Controls.Add(this.label2);
+            this.panelDraw.Controls.Add(this.labelInstDist);
+            this.panelDraw.Controls.Add(this.label1);
+            this.panelDraw.Controls.Add(this.labelMaxHight);
+            this.panelDraw.Controls.Add(this.labelHight);
+            this.panelDraw.Controls.Add(this.labelHautInst);
             this.panelDraw.Location = new System.Drawing.Point(12, 119);
             this.panelDraw.Name = "panelDraw";
             this.panelDraw.Size = new System.Drawing.Size(670, 445);
             this.panelDraw.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(496, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Distance Max [m] :";
+            // 
+            // labelMaxLenght
+            // 
+            this.labelMaxLenght.AutoSize = true;
+            this.labelMaxLenght.Location = new System.Drawing.Point(606, 115);
+            this.labelMaxLenght.Name = "labelMaxLenght";
+            this.labelMaxLenght.Size = new System.Drawing.Size(0, 13);
+            this.labelMaxLenght.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(496, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Distance Inst [m] :";
+            // 
+            // labelInstDist
+            // 
+            this.labelInstDist.AutoSize = true;
+            this.labelInstDist.Location = new System.Drawing.Point(606, 88);
+            this.labelInstDist.Name = "labelInstDist";
+            this.labelInstDist.Size = new System.Drawing.Size(0, 13);
+            this.labelInstDist.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(496, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hauteur Max [m] :";
+            // 
+            // labelMaxHight
+            // 
+            this.labelMaxHight.AutoSize = true;
+            this.labelMaxHight.Location = new System.Drawing.Point(606, 47);
+            this.labelMaxHight.Name = "labelMaxHight";
+            this.labelMaxHight.Size = new System.Drawing.Size(0, 13);
+            this.labelMaxHight.TabIndex = 2;
+            // 
+            // labelHight
+            // 
+            this.labelHight.AutoSize = true;
+            this.labelHight.Location = new System.Drawing.Point(496, 19);
+            this.labelHight.Name = "labelHight";
+            this.labelHight.Size = new System.Drawing.Size(88, 13);
+            this.labelHight.TabIndex = 1;
+            this.labelHight.Text = "Hauteur Inst [m] :";
+            // 
+            // labelHautInst
+            // 
+            this.labelHautInst.AutoSize = true;
+            this.labelHautInst.Location = new System.Drawing.Point(606, 19);
+            this.labelHautInst.Name = "labelHautInst";
+            this.labelHautInst.Size = new System.Drawing.Size(0, 13);
+            this.labelHautInst.TabIndex = 0;
             // 
             // Form1
             // 
@@ -118,9 +223,10 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Tire Balistique";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.panelDraw.ResumeLayout(false);
+            this.panelDraw.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -135,6 +241,16 @@
         private System.Windows.Forms.Label labelDegrees;
         private System.Windows.Forms.Timer timerTemp;
         private System.Windows.Forms.Panel panelDraw;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label labelHautInst;
+        private System.Windows.Forms.Label labelHight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelMaxHight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelMaxLenght;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelInstDist;
     }
 }
 
--- Modules/mTirBalistique/Tire-Balistique.cs
+++ Modules/mTirBalistique/Tire-Balistique.cs
@@ -1,4 +1,32 @@
-﻿using System;
+﻿// : définit le point d'entrée pour l'application console.
+/*****************************************************************************
+** PROGRAMME   Tir balistique                                                **
+**                                                                           **
+** Lieu      : ETML - section informatique                                   **
+** Auteur    : Maxime Pichonnat                                              **
+** Date      : 04.05.2016                                                    **
+**                                                                           **
+** Modifications                                                             **
+**   Auteur  :                                                               **
+**   Version : X.X                                                           **
+**   Date    :                                                               **
+**   Raisons :                                                               **
+**                                                                           **
+**                                                                           **
+******************************************************************************/
+
+/******************************************************************************
+** DESCRIPTION                                                               
+** Programme qui calcul un tire balistique basique (vitesse, gravité et angle)                                                                        
+**                                                                           
+**                                                                           
+******************************************************************************/
+
+// ******************************************************************* 
+// Nom   : Tir balistique 
+// But   : Arriver à la fin 
+// *******************************************************************
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,18 +45,43 @@ namespace Tire_balistique
         }
 
         bool textButton = false;
+        bool restart = false;
 
-  
+        double GravEarth = -9.81;
 
+        double SpeedY;
+        double SpeedX;
+
+        double angle = 0;
+        double speed = 0;
+
+        double x, y;
+
+        const int SIZE_X = 670;
+        const int SIZE_Y = 445;
+
+        Color colorShoot = Color.Red;
+        SolidBrush myBrush;
+
+        int X = 0;
+
+        
+        /// <summary>
+        /// Appelle les méthodes et change le text des boutons  
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonStop_Click(object sender, EventArgs e)
         {
             if (textButton == false)
             {
                 buttonStop.Text = "Pause";
                 textButton = true;
+                textBoxDegrees.Enabled = false;
+                textBoxSpeed.Enabled = false;
+
+                calValues();
                 timerTemp.Start();
-                calBalistique();
-                
             }
             else
             {
@@ -36,61 +89,168 @@ namespace Tire_balistique
                 textButton = false;
                 timerTemp.Stop();
             }
-
-            
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
+        
+        //Timer 
         /// <summary>
-        /// Calcule le tire balistique de l'object
+        /// Calcul le tire balistique, affiche le tire balistique et affiche la distance et la hauteur à la quelle le tire se trouve
         /// </summary>
-        private void calBalistique()
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timerTemp_Tick(object sender, EventArgs e)
         {
-            double GravEarth = -9.81;
+            x += 0.01;
 
-            double SpeedY;
-            double SpeedX;
-            double x, y;
-
-            int X = 0;
             int Y = panelDraw.Height;
 
-            double speed = Convert.ToDouble(textBoxSpeed.Text);
-            double angle = Convert.ToDouble(textBoxDegrees.Text);
+            //Vérifier que les cases soient bien ramplie 
+                if (textBoxDegrees.Text == "")
+                {
+                    textButton = true;
+                    textBoxDegrees.BackColor = Color.Red;
+                }
+                else
+                {
+                    textBoxDegrees.BackColor = Color.White;
+                    angle = Convert.ToDouble(textBoxDegrees.Text);
+                   
+                }
 
+                //Vérifier que les cases soient bien ramplie 
+                if (textBoxSpeed.Text == "")
+                {
 
+                    textBoxSpeed.BackColor = Color.Red;
+                }
+                else
+                {
+                    textBoxSpeed.BackColor = Color.White;
+                    speed = Convert.ToDouble(textBoxSpeed.Text);
+                   
+                }
 
             //Création du graphique
-            Bitmap pen = new Bitmap(4, 4);
-            pen.SetPixel(0, 0, Color.Red);
+            //Bitmap pen = new Bitmap(30, 30);
+            //pen.SetPixel(0, 0, colorShoot);
+            myBrush = new SolidBrush(buttonColor.BackColor);
             System.Drawing.Graphics graphBali = panelDraw.CreateGraphics();
+
+            if (restart == true)
+            {
+                graphBali.Clear(Color.White);
+                restart = false;
+            }
+            
 
             //Vy Calcul la vitesse montante
             SpeedY = Math.Sin(Math.PI * (angle / 180)) * speed;
-            
 
-            //Calcul balistique du tire
-            for (double i = 0; i < 10000; i += 0.01)
+            //Vx Calcul la vitesse horizontale
+            SpeedX = Math.Cos(Math.PI * (angle / 180)) * speed;
+
+            
+            y = SpeedY * x + ((0.5 * (GravEarth)) * (x * x));
+            
+            //Compteur de vitesse et de distance
+            if (y < 0)
+            {
+                labelHautInst.Text = "0";
+            }
+            else
+            {
+                labelHautInst.Text = Convert.ToString(Math.Round(y, 2));
+            }
+            
+            labelInstDist.Text = Convert.ToString(Math.Round(SpeedX * x, 2));
+
+            //graphBali.DrawImageUnscaled(pen, X + Convert.ToInt32(SpeedX * x), Y - Convert.ToInt32(y));
+
+            graphBali.FillRectangle(myBrush, X + Convert.ToInt32(SpeedX * x), Y - Convert.ToInt32(y) -6, 6, 6);
+            
+            
+            if (Y - Convert.ToInt32(y) > SIZE_Y)
+            {
+                timerTemp.Stop();
+                buttonStop.Text = "Départ";
+                restart = true;
+                textButton = false;
+                x = 0;
+                y = 0;
+                SpeedY = 0;
+                SpeedX = 0;
+
+                textBoxDegrees.Enabled = true;
+                textBoxSpeed.Enabled = true;
+            }
+        }
+        /// <summary>
+        /// Permet de changer la couleur du tire balistique 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                buttonColor.BackColor = colorDialog1.Color;
+                colorShoot = colorDialog1.Color;
+            }
+        }
+
+        /// <summary>
+        /// Calcul en avance la distance max et la hauteur max
+        /// </summary>
+        private void calValues()
+        {
+            int Y = panelDraw.Height;
+
+            double a;
+            double b;
+
+            double height = 0;
+            double lenght = 0;
+
+            angle = Convert.ToDouble(textBoxDegrees.Text);
+            speed = Convert.ToDouble(textBoxSpeed.Text);
+
+            //Vy Calcul la vitesse montante
+            SpeedY = Math.Sin(Math.PI * (angle / 180)) * speed;
+
+
+            for (double i = 0; ; i += 0.01)
             {
                 //Vx Calcul la vitesse horizontale
                 SpeedX = Math.Cos(Math.PI * (angle / 180)) * speed;
 
-                x = i;
-                y = SpeedY * i + ((0.5 * (GravEarth)) * (i * i));
+                a = i;
+                b = SpeedY * i + ((0.5 * (GravEarth)) * (i * i));
 
-                graphBali.DrawImageUnscaled(pen, X + Convert.ToInt32(SpeedX * i), Y - Convert.ToInt32(y));
+                if (height <= (Math.Round(b, 2)))
+                {
+                    height = Math.Round(b, 2);
+                }
 
+                lenght = Math.Round(SpeedX * a, 2);
+
+                if (Y - Convert.ToInt32(b) > SIZE_Y)
+                {
+                    break;
+                }
             }
+            //Affiche la distance Max et la hauteur Max
+            labelMaxHight.Text = Convert.ToString(Math.Round(height, 2));
+            labelMaxLenght.Text = Convert.ToString(Math.Round(lenght, 2));
         }
 
-        //Timer 
-        private void timerTemp_Tick(object sender, EventArgs e)
+        private void textBoxDegrees_TextChanged(object sender, EventArgs e)
         {
-           
+            if (!System.Text.RegularExpressions.Regex.IsMatch(textBoxDegrees.Text, "^[0-9]+$")) { textBoxDegrees.Text = "0"; }
+        }
+
+        private void textBoxSpeed_TextChanged(object sender, EventArgs e)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(textBoxSpeed.Text, "^[0-9]+$")) { textBoxSpeed.Text = "0"; }
         }
     }
 }
