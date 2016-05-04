95bcb7c43609e897566bfcd4df52286e6bff130d
ReyQu <quentin.rey8@gmail.com>
2016-05-04 15:35:17 +0200

    Added files via upload

---
+++ Modules/mStructRepertoire/mStructRepertoire/ModuleStructure.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace ModuleStructure
+{
+    partial class ModuleStructure
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModuleStructure));
+            this.TextZonegrpBx = new System.Windows.Forms.GroupBox();
+            this.FolderStructureRichTxtB = new System.Windows.Forms.RichTextBox();
+            this.SearchFileGrpBx = new System.Windows.Forms.GroupBox();
+            this.SelectedPathTxtBox = new System.Windows.Forms.TextBox();
+            this.SearchFileBtn = new System.Windows.Forms.Button();
+            this.HelpGrpBx = new System.Windows.Forms.GroupBox();
+            this.HelpLbl = new System.Windows.Forms.Label();
+            this.CreateStructureBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CreationProgressPrB = new System.Windows.Forms.ProgressBar();
+            this.SearchFileFBDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.ProgressionLbl = new System.Windows.Forms.Label();
+            this.ProgressOfProgressBarLbl = new System.Windows.Forms.Label();
+            this.TextZonegrpBx.SuspendLayout();
+            this.SearchFileGrpBx.SuspendLayout();
+            this.HelpGrpBx.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextZonegrpBx
+            // 
+            this.TextZonegrpBx.Controls.Add(this.FolderStructureRichTxtB);
+            this.TextZonegrpBx.Location = new System.Drawing.Point(22, 12);
+            this.TextZonegrpBx.Name = "TextZonegrpBx";
+            this.TextZonegrpBx.Size = new System.Drawing.Size(383, 302);
+            this.TextZonegrpBx.TabIndex = 0;
+            this.TextZonegrpBx.TabStop = false;
+            this.TextZonegrpBx.Text = "Zone de saisie";
+            // 
+            // FolderStructureRichTxtB
+            // 
+            this.FolderStructureRichTxtB.Location = new System.Drawing.Point(16, 19);
+            this.FolderStructureRichTxtB.Name = "FolderStructureRichTxtB";
+            this.FolderStructureRichTxtB.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.FolderStructureRichTxtB.Size = new System.Drawing.Size(361, 277);
+            this.FolderStructureRichTxtB.TabIndex = 0;
+            this.FolderStructureRichTxtB.Text = "";
+            // 
+            // SearchFileGrpBx
+            // 
+            this.SearchFileGrpBx.Controls.Add(this.SelectedPathTxtBox);
+            this.SearchFileGrpBx.Controls.Add(this.SearchFileBtn);
+            this.SearchFileGrpBx.Location = new System.Drawing.Point(22, 327);
+            this.SearchFileGrpBx.Name = "SearchFileGrpBx";
+            this.SearchFileGrpBx.Size = new System.Drawing.Size(383, 66);
+            this.SearchFileGrpBx.TabIndex = 1;
+            this.SearchFileGrpBx.TabStop = false;
+            this.SearchFileGrpBx.Text = "Parcourir";
+            // 
+            // SelectedPathTxtBox
+            // 
+            this.SelectedPathTxtBox.Location = new System.Drawing.Point(6, 28);
+            this.SelectedPathTxtBox.Name = "SelectedPathTxtBox";
+            this.SelectedPathTxtBox.Size = new System.Drawing.Size(229, 20);
+            this.SelectedPathTxtBox.TabIndex = 5;
+            // 
+            // SearchFileBtn
+            // 
+            this.SearchFileBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SearchFileBtn.Location = new System.Drawing.Point(255, 28);
+            this.SearchFileBtn.Name = "SearchFileBtn";
+            this.SearchFileBtn.Size = new System.Drawing.Size(121, 22);
+            this.SearchFileBtn.TabIndex = 4;
+            this.SearchFileBtn.Text = "Parcourir";
+            this.SearchFileBtn.UseVisualStyleBackColor = true;
+            this.SearchFileBtn.Click += new System.EventHandler(this.SearchFileBtn_Click);
+            // 
+            // HelpGrpBx
+            // 
+            this.HelpGrpBx.Controls.Add(this.HelpLbl);
+            this.HelpGrpBx.Location = new System.Drawing.Point(448, 13);
+            this.HelpGrpBx.Name = "HelpGrpBx";
+            this.HelpGrpBx.Size = new System.Drawing.Size(216, 295);
+            this.HelpGrpBx.TabIndex = 2;
+            this.HelpGrpBx.TabStop = false;
+            this.HelpGrpBx.Text = "Aide";
+            // 
+            // HelpLbl
+            // 
+            this.HelpLbl.AutoSize = true;
+            this.HelpLbl.Location = new System.Drawing.Point(3, 16);
+            this.HelpLbl.Name = "HelpLbl";
+            this.HelpLbl.Size = new System.Drawing.Size(196, 143);
+            this.HelpLbl.TabIndex = 0;
+            this.HelpLbl.Text = resources.GetString("HelpLbl.Text");
+            // 
+            // CreateStructureBtn
+            // 
+            this.CreateStructureBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CreateStructureBtn.Location = new System.Drawing.Point(448, 327);
+            this.CreateStructureBtn.Name = "CreateStructureBtn";
+            this.CreateStructureBtn.Size = new System.Drawing.Size(216, 29);
+            this.CreateStructureBtn.TabIndex = 1;
+            this.CreateStructureBtn.Text = "Créer";
+            this.CreateStructureBtn.UseVisualStyleBackColor = true;
+            this.CreateStructureBtn.Click += new System.EventHandler(this.CreateStructureBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.CreationProgressPrB);
+            this.panel1.Location = new System.Drawing.Point(448, 362);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(216, 31);
+            this.panel1.TabIndex = 3;
+            // 
+            // CreationProgressPrB
+            // 
+            this.CreationProgressPrB.Location = new System.Drawing.Point(-2, -2);
+            this.CreationProgressPrB.Name = "CreationProgressPrB";
+            this.CreationProgressPrB.Size = new System.Drawing.Size(216, 31);
+            this.CreationProgressPrB.TabIndex = 4;
+            // 
+            // ProgressionLbl
+            // 
+            this.ProgressionLbl.AutoSize = true;
+            this.ProgressionLbl.BackColor = System.Drawing.Color.Transparent;
+            this.ProgressionLbl.ForeColor = System.Drawing.Color.Black;
+            this.ProgressionLbl.Location = new System.Drawing.Point(445, 396);
+            this.ProgressionLbl.Name = "ProgressionLbl";
+            this.ProgressionLbl.Size = new System.Drawing.Size(71, 13);
+            this.ProgressionLbl.TabIndex = 4;
+            this.ProgressionLbl.Text = "Progression : ";
+            // 
+            // ProgressOfProgressBarLbl
+            // 
+            this.ProgressOfProgressBarLbl.AutoSize = true;
+            this.ProgressOfProgressBarLbl.BackColor = System.Drawing.Color.Transparent;
+            this.ProgressOfProgressBarLbl.ForeColor = System.Drawing.Color.Black;
+            this.ProgressOfProgressBarLbl.Location = new System.Drawing.Point(513, 396);
+            this.ProgressOfProgressBarLbl.Name = "ProgressOfProgressBarLbl";
+            this.ProgressOfProgressBarLbl.Size = new System.Drawing.Size(0, 13);
+            this.ProgressOfProgressBarLbl.TabIndex = 5;
+            // 
+            // ModuleStructure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(687, 414);
+            this.Controls.Add(this.ProgressOfProgressBarLbl);
+            this.Controls.Add(this.ProgressionLbl);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.CreateStructureBtn);
+            this.Controls.Add(this.HelpGrpBx);
+            this.Controls.Add(this.SearchFileGrpBx);
+            this.Controls.Add(this.TextZonegrpBx);
+            this.Name = "ModuleStructure";
+            this.Text = "ModuleStructure";
+            this.TextZonegrpBx.ResumeLayout(false);
+            this.SearchFileGrpBx.ResumeLayout(false);
+            this.SearchFileGrpBx.PerformLayout();
+            this.HelpGrpBx.ResumeLayout(false);
+            this.HelpGrpBx.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox TextZonegrpBx;
+        private System.Windows.Forms.RichTextBox FolderStructureRichTxtB;
+        private System.Windows.Forms.GroupBox SearchFileGrpBx;
+        private System.Windows.Forms.GroupBox HelpGrpBx;
+        private System.Windows.Forms.Label HelpLbl;
+        private System.Windows.Forms.Button CreateStructureBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button SearchFileBtn;
+        private System.Windows.Forms.FolderBrowserDialog SearchFileFBDialog;
+        private System.Windows.Forms.TextBox SelectedPathTxtBox;
+        private System.Windows.Forms.ProgressBar CreationProgressPrB;
+        private System.Windows.Forms.Label ProgressionLbl;
+        private System.Windows.Forms.Label ProgressOfProgressBarLbl;
+
+
+
+    }
+}
+
---
+++ Modules/mStructRepertoire/mStructRepertoire/ModuleStructure.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ModuleStructure
+{
+    public partial class ModuleStructure : Form
+    {
+        public ModuleStructure()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchFileBtn_Click(object sender, EventArgs e)
+        {
+            SearchFileFBDialog.ShowDialog();
+            SelectedPathTxtBox.Text = SearchFileFBDialog.SelectedPath;
+        }
+
+        private void CreateStructureBtn_Click(object sender, EventArgs e)
+        {
+            int Tabnumber;
+            string UserSelectedPath = SelectedPathTxtBox.Text;
+            string[] tab_TextBoxLine = FolderStructureRichTxtB.Lines;
+            int[] tab_TabPerLine = new int[tab_TextBoxLine.Length];
+            string[] tab_Path = new string[tab_TextBoxLine.Length];
+            string CompletePath = UserSelectedPath;
+            string TempPath = "";
+            string DefaultPath = SelectedPathTxtBox.Text;
+            string DirectoryName;
+            double ProgressBarValue;
+            double ProgressBarMax;
+            double DblPercentage;
+
+
+            if (SelectedPathTxtBox.Text != "")
+            {
+                if (FolderStructureRichTxtB.Text != "")
+                {
+                    if (Directory.Exists(DefaultPath))
+                    {
+                        CreationProgressPrB.Maximum = tab_TextBoxLine.Length;
+                        CreationProgressPrB.Minimum = 0;
+                        CreationProgressPrB.Step = 1;
+                        CreationProgressPrB.Value = 0;
+
+                        for (int intX = 0; intX < tab_TextBoxLine.Length; intX++)
+                        {
+                            Tabnumber = 0;
+                            DirectoryName = tab_TextBoxLine[intX];
+                            bool IsLetter = false;
+
+
+                            for (int intY = 0; intY < DirectoryName.Length; intY++)
+                            {
+                                if (DirectoryName[intY] == Convert.ToChar("\t") && IsLetter == false)
+                                {
+                                    Tabnumber++;
+                                }
+                                else
+                                    IsLetter = true;
+                            }
+
+                            tab_TabPerLine[intX] = Tabnumber;
+
+                            if (tab_TabPerLine[intX] == 0)
+                            {
+                                string DeleteTabulation = "";
+                                TempPath = "";
+                                bool NoNewLine = false;
+
+                                for (int intA = 0; intA < DirectoryName.Length; intA++)
+                                {
+                                    if (DirectoryName[intA] != Convert.ToChar("\t"))
+                                    {
+                                        DeleteTabulation += DirectoryName[intA];
+                                    }
+                                }
+
+                                CompletePath = DefaultPath;
+                                CompletePath += "\\" + DeleteTabulation;
+                                tab_Path[intX] = CompletePath;
+
+                                /*CompletePath = DefaultPath;
+                                CompletePath += "\\" + DirectoryName;
+                                tab_Path[intX] = CompletePath;*/
+
+                                /*CompletePath += TempPath;
+                                tab_Path[intX] = CompletePath;*/
+
+                                //break;
+                                    
+                                
+                            }
+                            else
+                            {
+                                for (int intZ = intX; intZ >= 0; intZ--)
+                                {
+                                    if ((tab_TabPerLine[intZ] < (tab_TabPerLine[intX])))
+                                    {
+                                        string DeleteTabulation = "";
+                                        TempPath = "";
+                                        bool NoNewLine = false;
+
+                                        for (int intA = 0; intA < DirectoryName.Length; intA++)
+                                        {
+                                            if (DirectoryName[intA] != Convert.ToChar("\t"))
+                                            {
+                                                DeleteTabulation += DirectoryName[intA];
+                                            }/*
+                                            else
+                                            {
+                                                DeleteTabulation += DirectoryName[intA];
+                                                noNewLine = true;
+                                            }*/
+                                        }
+                                        TempPath = TempPath + "\\" + DeleteTabulation;
+
+                                        CompletePath += TempPath;
+                                        tab_Path[intX] = CompletePath;
+
+                                        break;
+                                    }
+                                }
+                            }
+
+                            for (int intY = intX; intY >= 0; intY--)
+                            {
+                                if (tab_TabPerLine[intY] <= tab_TabPerLine[intX])
+                                {
+                                    if (tab_TabPerLine[intY] < (tab_TabPerLine[intX]))
+                                    {
+                                        string DeleteTabulation = "";
+                                        TempPath = "";
+
+                                        for (int intA = 0; intA < DirectoryName.Length; intA++)
+                                        {
+                                            if (DirectoryName[intA] != Convert.ToChar("\t"))
+                                            {
+                                                DeleteTabulation += DirectoryName[intA];
+                                            }
+                                        }
+
+                                        TempPath = TempPath + "\\" + DeleteTabulation;
+
+                                        tab_Path[intX] = tab_Path[intY] + "\\" + TempPath;
+
+                                        break;
+                                    }
+                                }
+                            }
+
+                            System.IO.Directory.CreateDirectory(tab_Path[intX]);
+                            if (CreationProgressPrB.Value < CreationProgressPrB.Maximum)
+                                CreationProgressPrB.Value += CreationProgressPrB.Step;
+
+                            else
+                                CreationProgressPrB.Value = CreationProgressPrB.Maximum;
+
+
+                            ProgressBarValue = CreationProgressPrB.Value;
+                            ProgressBarMax = CreationProgressPrB.Maximum;
+                            DblPercentage = Math.Round(Convert.ToDouble((ProgressBarValue * 100) / ProgressBarMax), 2);
+                            ProgressOfProgressBarLbl.Text = Convert.ToString(DblPercentage) + " %";
+                        }
+
+                        /*for (int intX = 0; intX < tab_TextBoxLine.Length; intX++)
+                        {
+                            DirectoryName = tab_TextBoxLine[intX];
+
+                            for (int intY = intX; intY >= 0; intY--)
+                            {
+                                if (tab_TabPerLine[intY] <= tab_TabPerLine[intX])
+                                {
+                                    if (tab_TabPerLine[intY] < (tab_TabPerLine[intX]))
+                                    {
+                                        string DeleteTabulation = "";
+                                        TempPath = "";
+
+                                        for (int intA = 0; intA < DirectoryName.Length; intA++)
+                                        {
+                                            if (DirectoryName[intA] != Convert.ToChar("\t"))
+                                            {
+                                                DeleteTabulation += DirectoryName[intA];
+                                            }
+                                        }
+
+                                        TempPath = TempPath + "\\" + DeleteTabulation;
+
+                                        tab_Path[intX] = tab_Path[intY] + "\\" + TempPath;
+
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                        for (int intX = 0; intX < tab_TextBoxLine.Length; intX++)
+                        {
+                            System.IO.Directory.CreateDirectory(tab_Path[intX]);
+                            if (CreationProgressPrB.Value < CreationProgressPrB.Maximum)
+                                CreationProgressPrB.Value += CreationProgressPrB.Step;
+                        }*/
+                    }
+                    else
+                    {
+                        MessageBox.Show("Dossier inexistant ! Vérifiez bien que votre dossier possède la bonne orthographe ou qu'il existe !");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez créer au moins un dossier !");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner un chemin !");
+            }
+        }
+    }
+}
